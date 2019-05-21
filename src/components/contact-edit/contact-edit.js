5ca611c512ff4e344c8908c8aa02d805c40839f3
Denis Vetrov <denisvetrov@MacBook-Air-Denis.local>
2019-05-21 12:42:42 +0300

    Add logic for contact-edit component

--- src/components/contact-edit/contact-edit.js
+++ src/components/contact-edit/contact-edit.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { default_image_url } from "../../constants/constants"
 import logo from '../../assets/images/logo.svg';
 import './styles.css';
 //components
@@ -7,35 +8,68 @@ class ContactEdit extends React.Component {
     state = {
         name: "",
         surname: "",
-        phone: ""
+        phone: "",
+        image_url: "",
+        call_history: []
     }
     handleChange = (event) => {
+        console.log(event.target.value)
         if (event.target.value.length > 0) {
-            this.setState({ [event.target.name]: parseFloat(event.target.value) })
+            this.setState({ [event.target.name]: event.target.value })
         }
     }
+    submitForm = () => {
+        const { name, surname, phone, image_url } = this.state
+        console.log(this.state)
+    }
     render() {
-        const { phones } = this.state
+        const { name, surname, phone, image_url, call_history } = this.state
         return (
             <div className="ContactEdit">
                 <form >
+                    <div className="ContactEdit-image-container">
+                        <div className="ContactEdit-image" style={{
+                            backgroundImage: `url(${image_url ? image_url : default_image_url})`,
+                            backgroundSize: "cover"
+                        }} />
+                        <input type="file" name="image_url" accept="image/*" onChange={this.handleChange}></input>
+                    </div>
                     <div className="ContactEdit-inputs">
                         <div>
                             <p>Name:</p>
-                            <input type="text" name="name" disabled={true} />
+                            <input type="text" name="name" value={name} disabled={false} onChange={this.handleChange} />
                         </div>
                         <div>
                             <p>Surname:</p>
-                            <input type="text" name="surname" disabled={true} />
+                            <input type="text" name="surname" value={surname} disabled={true} onChange={this.handleChange} />
                         </div>
                         <div>
                             <p>Phone:</p>
-                            <input type="text" name="phone" disabled={true} />
+                            <input type="text" name="phone" value={phone} disabled={true} onChange={this.handleChange} />
                         </div>
                     </div>
-                    <div className="ContactEdit-submit" onClick={() => { }}>
-                        Submit
+                    {call_history.length > 0 &&
+                        <div className="ContactEdit-call-history">
+                            <div className="Call-history-line Call-history-header">
+                                <p>Name:</p>
+                                <p>Phone:</p>
+                                <p>Date:</p>
+                            </div>
+                            {call_history.map((call, index) => {
+                                const { name, phone, date } = call
+                                return (
+                                    <div className="Call-history-line">
+                                        <p>{name}</p>
+                                        <p>{phone}</p>
+                                        <p>{date}</p>
+                                    </div>
+                                )
+                            })}
                         </div>
+                    }
+                    <div className="ContactEdit-submit" onClick={() => { this.submitForm() }}>
+                        Save
+                    </div>
                 </form>
             </div>
         );
